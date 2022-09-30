21c0067ff6c2e670e059b5f77d72e3a70eb864dc
alxndrsn <alxndrsn>
2022-09-30 16:55:35 +0300

    Is it a bug?

---
+++ index.js
@@ -0,0 +1,42 @@
+const { Transform } = require('node:stream');
+const pg = require('pg');
+const QueryStream = require('pg-query-stream');
+
+const pool = new pg.Pool({
+  user:  'jubilant',
+  password: 'jubilant',
+  poolSize: 1,
+  connectionTimeoutMillis: 400,
+  statement_timeout: 400,
+});
+
+(async () => {
+  console.log('Starting...');
+  const query = new QueryStream('SELECT * FROM generate_series(0, 100000) AS n');
+  console.log('Querying...');
+  const client = await pool.connect();
+  const stream = await client.query(query);
+  console.log('Got stream');
+
+  console.log('Running simple query (which should time out)...');
+  try {
+    const res = await pool.query('SELECT TRUE');
+    throw new Error(`Unexpected res (should have failed): ${JSON.stringify(res, null, 2)}`);
+  } catch(err) {
+    if(err.message !== 'timeout exceeded when trying to connect') throw new Error(`Unexpected failure: ${err}`);
+    console.log('Failed as expected.');
+  }
+
+  await stream.destroy(); // without this, next attempt at pool.query() will hang
+  await client.release();
+  console.log('Destroyed.');
+
+  console.log('Querying again...');
+  const res = await pool.query('SELECT TRUE AS val');
+  if(res.rows.length !== 1 || res.rows[0].val !== true) {
+    throw new Error('Unexpected res: ' + JSON.stringify(res.rows));
+  } else {
+    console.log('Got expected result.');
+  }
+  console.log('Completed OK.');
+})();
